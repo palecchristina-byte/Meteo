e1ae440b3c96c9f47e6d6029f95a072b79c5dffe
palecchristina-byte <palecchristina@gmail.com>
2025-12-15 22:54:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ function displayForecast(response) {
         forecastHtml +
         `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
 
         <img src= "${day.condition.icon_url}" class="weather-forecast-icon"/>
         </div>
