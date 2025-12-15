45285b7333e9988aecb78172f9b79a40fd858e3e
palecchristina-byte <palecchristina@gmail.com>
2025-12-15 23:03:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -100,4 +100,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Italy");
-gerForecast("Italy");
+getForecast("Italy");
