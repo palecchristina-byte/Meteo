d1b37a6471b38a9efecc192f0eb99bf6d17efdf6
palecchristina-byte <palecchristina@gmail.com>
2025-12-15 23:14:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -78,7 +78,6 @@ function displayForecast(response) {
       <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <img src= "${day.condition.icon_url}" class="weather-forecast-icon"/>
-        </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
             <strong>${Math.round(day.temperature.maximum)}º</strong>
